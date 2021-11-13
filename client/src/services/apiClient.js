f3288b179f27dd676308471a7514ba43914f1883
Stephen Wuh <swuh87@gmail.com>
2021-11-13 16:06:35 -0500

    api endpoint updated

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -4,25 +4,25 @@ const apiClient = {
 
     getApps: async () => {
 
-        let response = await axios.get('http://localhost:3001/api/getApps');
+        let response = await axios.get('/api/getApps');
 
         return response.data
     },
     updateJobApp: async (jobAppId, updatedJobApp) => {
 
-        let response = await axios.put(`http://localhost:3001/api/updateJobApp/${jobAppId}`, updatedJobApp);
+        let response = await axios.put(`/api/updateJobApp/${jobAppId}`, updatedJobApp);
 
         return response.data
     },
     addJobApp: async (newJobApp) => {
 
-        let response = await axios.post(`http://localhost:3001/api/addJobApp/`, newJobApp);
+        let response = await axios.post(`/api/addJobApp/`, newJobApp);
 
         return response.data
     },
     deleteJobApp: async (jobAppId) => {
 
-        let response = await axios.delete(`http://localhost:3001/api/deleteJobApp/${jobAppId}`);
+        let response = await axios.delete(`/api/deleteJobApp/${jobAppId}`);
 
         return response.data
     },
