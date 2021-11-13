23464318819d7b651adba7c6ca2c5fa54a44a94e
Stephen Wuh <swuh87@gmail.com>
2021-11-13 14:56:11 -0500

    script updated

--- controller.js
+++ controller.js
@@ -1,51 +1,45 @@
-let jobApps = require('./db.json')
+let jobApps = require("./db.json");
 
 globalId = 2;
 
 module.exports = {
-    getApps: (_req, res) => {
-        res.send(jobApps);
-    },
+  getApps: (_req, res) => {
+    res.send(jobApps);
+  },
 
-    updateJobApp: (req, res)=> {
-        const jobAppId = parseInt(req.params.id);
+  updateJobApp: (req, res) => {
+    const jobAppId = parseInt(req.params.id);
 
-        for(let i=0; i < jobApps.length; i++){
-           if(jobApps[i].id === jobAppId){
-               jobApps.splice(i,1, req.body)
-           }
-        }
+    for (let i = 0; i < jobApps.length; i++) {
+      if (jobApps[i].id === jobAppId) {
+        jobApps.splice(i, 1, req.body);
+      }
+    }
 
-    res.send('update successfull');
+    res.send("update successfull");
+  },
 
-    },
-
-    addJobApp: (req, res)=> {
-
+  addJobApp: (req, res) => {
     let newJobApp = req.body;
 
     newJobApp.id = globalId;
 
-    jobApps.push(newJobApp)
+    jobApps.push(newJobApp);
 
-    globalId ++
+    globalId++;
 
+    res.send("Job App added successfully");
+  },
 
-    res.send('Job App added successfully')
+  deleteJobApp: (req, res) => {
+    const jobAppId = parseInt(req.params.id);
 
-    },
-
-    deleteJobApp: (req, res)=> {
-
-        const jobAppId = parseInt(req.params.id);
-
-        for(let i=0; i < jobApps.length; i++){
-            if(jobApps[i].id === jobAppId){
-                jobApps.splice(i,1)
-            }
-         }
-
-        res.send('Job App deleted successfully')
+    for (let i = 0; i < jobApps.length; i++) {
+      if (jobApps[i].id === jobAppId) {
+        jobApps.splice(i, 1);
+      }
     }
 
-}
+    res.send("Job App deleted successfully");
+  },
+};
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const ctrl = require('./controller.js')
+const ctrl = require('./controller.js');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/getApps', ctrl.getApps);
 app.put('/api/updateJobApp/:id', ctrl.updateJobApp);
 app.post('/api/addJobApp/', ctrl.addJobApp);
 app.delete('/api/deleteJobApp/:id', ctrl.deleteJobApp);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+ '/client/build/index.html'));
+});
 
 const port = process.env.PORT || 3001;
 
